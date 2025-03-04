01ee0b869240e29aabe58c8a966469b70366cb49
ASCOLBIT <bitascol123@gmail.com>
2025-03-04 08:01:54 +0545

    color edited!

--- resources/views/edit-location.blade.php
+++ resources/views/edit-location.blade.php
@@ -6,11 +6,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Location</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <style></style>
     <style>
         *{
             padding: 0;
             margin: 0;
             box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
         }
 
         .container {
@@ -24,6 +29,7 @@
             border: .125rem solid;
             padding: 1.5rem 1rem;
             border-radius: .5rem;
+            min-width: 20rem;
         }
 
         .heading {
@@ -41,35 +47,37 @@
         .input {
             display: flex;
             align-items: center;
+            gap: 1rem;
         }
 
         .input label {
             position: absolute;
             transform: translateY(-2.25em);
-            background-color: black;
+            background-color: #007bff;
             color: whitesmoke;
-            padding: .25rem .5rem;
+            padding: .125rem .25rem;
             border-radius: .5rem;
             font-size: x-small;
         }
 
         .input input {
             padding: .5rem .75rem;
-            border: none;
-            border-bottom: .125rem solid;
+            border: .125rem solid black;
+            border-radius: .25rem;
+            width: 100%;
         }
 
         .update-btn {
             width: 100%;
             padding: .5rem 0;
-            background-color: transparent;
+            background-color: #007bff;
             border-radius: .25rem;
-            border: .125rem solid black;
+            color: whitesmoke;
+            border: .125rem solid;
         }
 
         .update-btn:hover {
-            background-color: black;
-            color: whitesmoke;
+            background-color: #009aff;
         }
     </style>
 </head>
